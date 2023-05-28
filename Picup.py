3e3c2910785baf94c03db5fae7a809da18a5e2c9
yiqf <yiqf2022@126.com>
2023-05-28 18:29:28 +0800

    fix issues with abnormal crashes

--- Picup.py
+++ Picup.py
@@ -8,15 +8,11 @@ Email:    yiqf2022@126.com
 import os
 import sys
 
-import PyQt5
-
 from picup.service import client
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication
+from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import QApplication
 
-dirname = os.path.dirname(PyQt5.__file__)
-plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
 
 if __name__ == '__main__':
     __version__ = "2.2.1"
